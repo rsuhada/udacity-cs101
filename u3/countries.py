db7320552d6cb541369197a89cf3f6c2e7534320
rsuhada <suhada.robert@gmail.com>
2012-08-13 22:29:59 +0200

    u3-lists

---
+++ u3/countries.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+if __name__ == '__main__':
+
+    countries = [['China', 'Beijing', 1350],
+                 ['India', 'Delhi', 1220],
+                 ['Romania', 'Bucharest', 21],
+                 ['United States', 'Washington', 307]]
+
+    print countries[1][1]
+    print float(countries[0][2])/countries[2][2]
